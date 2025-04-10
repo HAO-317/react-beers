25409b164e235413519df82903f074140e7e7332
HAO-317 <lihao317@gmail.com>
2025-04-10 09:18:15 +0200

    add 404

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,18 @@ import AllBeers from './pages/AllBeers';
 import BeerDetails from './pages/BeerDetails';
 import RandomBeer from './pages/RandomBeer';
 
+
+const NotFound = () => {
+  return (
+    <div style={{ padding: '80px 20px', textAlign: 'center', color: '#fff' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <a href="/" style={{ color: '#f5c518' }}>Back to Home</a>
+    </div>
+  );
+};
+
+
 const AppContent = () => {
   const location = useLocation();
   const showNavbar = location.pathname !== '/'; 
@@ -17,6 +29,7 @@ const AppContent = () => {
         <Route path="/beers" element={<AllBeers />} />
         <Route path="/beers/:beerId" element={<BeerDetails />} />
         <Route path="/random-beer" element={<RandomBeer />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
